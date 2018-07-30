08f656fc7c121b8bf802ae3c815d61ab90aa682a
兔子 <457341620@qq.com>
2018-07-30 23:36:48 +0800

    不会

--- webpack.config.js
+++ webpack.config.js
@@ -1,99 +1,43 @@
-<<<<<<< HEAD
-// entry             入口文件
-// output            出口文件
-// module/rules      模块
-// plugins           插件
-// devserver         热更新
-var path = require('path');
-
-module.exports = {
-  entry: './main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'foo.bundle.js'
-  },
-  module: {
-	  rules: [
-	    {
-	      test: /\.js$/,
-	      exclude: /(node_modules|bower_components)/,
-	      loader: 'babel-loader',
-	      query: {
-	        presets: ['es2015','react']
-	      }
-	    },
-	    {
-	        test: /\.css$/,
-	        use: [ 'style-loader', 'css-loader' ]
-	    },
-	    {
-	        test: /\.(png|jpg|gif)$/,
-	        use: [
-	          {
-	            loader: 'file-loader',
-	            options: {}
-	          }
-	        ]
-	      }
-	  ]
-	},
-	devServer: {
-	  contentBase: path.join(__dirname, "dist"),    //只能监听html文件
-	  compress: true,
-	  port: 9000,
-	  inline:true     //开启热更新
-	}
-};
-=======
-
+//entry  output  module/rules  plugins devserver
 
 var path = require('path');
 
 module.exports = {
-		//入口文件
-	  entry: './main.js',
-	  //出口文件
-	  output: {
-	    path: path.resolve(__dirname, 'dist'),
-	    //打包好以后的文件名
-	    filename: 'foo.bundle.js'
-	  },
-	  
-	  module: {
-		  rules: [
-		    {
-		      test: /\.js$/,
-		      exclude: /(node_modules|bower_components)/,
-		      loader: 'babel-loader',
-		      query: {
-		        presets: ['es2015','react']
-		      }
-		    },
-		     {
-		        test: /\.css$/,
-		        use: [ 'style-loader', 'css-loader' ]
-              },
-               {
-			        test: /\.(png|jpg|gif|webp)$/,
-			        use: [
-			          {
-			            loader: 'file-loader',
-			            options: {}
-			          }
-                         ]
-                     }
-		  ]
-	},
-	   devServer: {
-		  contentBase: path.join(__dirname, "dist"),
-		  compress: true,
-		  port: 9000,
-		  inline:true
-	}   
-	  
-  
-  
-  
-  
+    entry: './main.js',//  入口 记得和你根目录下的main匹配 起什么自己定
+    output: {//出口
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'foo.bundle.js'
+    },
+    module: {
+        rules: [//loaders要换成rules
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015','react']//还要解析react语法
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            },
+        ]
+    },
+        devServer: {//和moudel平级
+            contentBase: path.join(__dirname, "dist"),//监听dist没有
+            //index  要复制一份到dist
+            compress: true,
+            port: 9000,
+            inline:true//热更新
+        }
 };
->>>>>>> 5fcba63... 邵婷婷的第一次提交
