93031ad4d71686b542b15c1b7909637220177cf8
兔子 <457341620@qq.com>
2018-07-30 22:56:47 +0800

    邵婷婷的第一次提交
    (cherry picked from commit 5fcba630abb2894f5ec362edfa67a764470c5b9c)
    
    # Conflicts:
    #	dist/foo.bundle.js
    #	dist/index.html
    #	main.js
    #	package.json
    #	webpack.config.js
    
    (cherry picked from commit 00df12b)

---
+++ components/app.js
@@ -0,0 +1,23 @@
+import React from 'react' 
+
+class App extends React.Component{
+	constructor(props){
+		super(props)
+	}
+
+
+	render(){
+		return(
+			<div>
+			<h1>hello webpack</h1>
+			
+			<Banner/>
+			</div>
+		)
+	}
+}
+
+
+		
+
+export default App;
