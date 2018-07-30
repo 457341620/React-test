ae7018499260c899c5148975d3b9f8726cb9571f
lana <1258616985@qq.com>
2018-07-30 22:03:33 +0800

    wqj added pro second

---
+++ main.js
@@ -0,0 +1,8 @@
+//入口文件
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Foodlist from './components/foodlist'
+import './css/style.css'
+
+ReactDOM.render(<Foodlist/>,document.getElementById('app'))
